4b79262e03347101f186f7254e23c81b2e7af3ab
JoJoWitness <Jordano470@gmail.com>
2023-07-27 21:26:49 -0400

    Merge conflict

--- Activities/footer.js
+++ Activities/footer.js
@@ -42,12 +42,12 @@ function Footer()  {
     <div class="footer_R d-flex flex-column row-gap-3" >
         <h3 class="fs-5" style="color: #1E94B1;">Contact Us</h3>
         <div class="d-flex align-items-center p-2 column-gap-2" style="height: 22px;">
-            <img src="../assets/Icons/location_on_FILL1_wght300_GRAD0_opsz48.svg" style="color: #1E94B1; width: 18px;"/>
+            <img src="./assets/Icons/location_on_FILL1_wght300_GRAD0_opsz48.svg" style="color: #1E94B1; width: 18px;"/>
             <p class="m-0">Cancun, Quintana Roo, Mexico</p>
         </div>
     
         <div class="d-flex align-items-center p-2 column-gap-2" style="height: 22px;">
-            <img src="../assets/Icons/mail_FILL1_wght300_GRAD0_opsz48.svg" style="color: #1E94B1; width: 18px;"/>
+            <img src="./assets/Icons/mail_FILL1_wght300_GRAD0_opsz48.svg" style="color: #1E94B1; width: 18px;"/>
             <p class="m-0">palmbreakinternational@gmail.com</p>
         </div>
     
